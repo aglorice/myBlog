2873dfce4db6aead2323b1db765b34f8da2caa86
aglorice <401208941@qq.com>
2022-11-22 15:33:47 +0800

    评论区

--- src/components/common/comment/comment.vue
+++ src/components/common/comment/comment.vue
@@ -6,14 +6,32 @@
     <div class="comment-input">
 
       <span>评论</span>
-      <el-input type="textarea"
-                v-model="comment"
-                :rows="5"
-                resize='none'
-                placeholder="请输入内容"
-                :maxlength='max_input'
-      ></el-input>
-      <span>{{strlimit}}/{{max_input}}</span>
+      <div class="input-comment">
+        <el-input
+                  class="input-comment-main"
+                  type="textarea"
+                  v-model="comment"
+                  :rows="5"
+                  resize='none'
+                  placeholder="请输入内容"
+                  :maxlength='max_input'
+        ></el-input>
+        <div class="send-button">
+          <img src="@/assets/img/send.png" alt="">
+        </div>
+      </div>
+      <span style="float: right">{{strlimit}}/{{max_input}}</span>
+
+      <img src="@/assets/img/emoji.png" class="emojiImg" alt="" @click="changeShowEmoji">
+      <VEmojiPicker
+          id="emoji"
+          v-show="isShowEmoji"
+          class="emojiShow"
+          :include="['people']"
+          :showSearch="false"
+          @select="selectEmoji"
+      />
+
 
     </div>
 
@@ -21,14 +39,28 @@
 </template>
 
 <script>
+import { Picker } from "emoji-mart-vue"; //引入组件
 export default {
   name: `comment`,
   data(){
     return{
       comment:"",
       strlimit:'',
-      max_input:200 // 字数限制
+      max_input:200, // 字数限制
+      isShowEmoji:false,
     }
+  },
+  components:{
+    VEmojiPicker:Picker
+  },
+  methods:{
+    selectEmoji(e) {
+      this.comment += e.native;
+    },
+    changeShowEmoji(){
+      this.isShowEmoji = !this.isShowEmoji
+    }
+
   },
   watch:{
     comment:{
@@ -45,6 +77,15 @@ export default {
 </script>
 
 <style scoped>
+.input-comment-main {
+  width: 1050px;
+}
+.input-comment {
+    width: 1100px;
+}
+#emoji {
+  position: relative;
+}
 .comment {
   width: 1240px;
   height: auto;
@@ -54,10 +95,24 @@ export default {
 .comment h2 {
   font-family: siyuan,serif;
 }
+.emojiImg {
+  width: 25px;
+  height: 25px;
+}
 .comment-input {
-  width: 1100px;
+  width: 1050px;
   height: auto;
-  margin: 0 70px 0 70px;
+  margin: 0 170px 0 70px;
 
+}
+.send-button img {
+  width: 50px;
+  margin: 30px 0 0 25px;
+  height: auto;
+}
+.send-button{
+  float: right;
+  width: 50px;
+  height: 117px;
 }
 </style>
