c65e95a2ad2b362df10d5feba352f3171074105b
aglorice <401208941@qq.com>
2022-12-06 22:42:36 +0800

    实现了归档功能

--- src/views/filetime/filetime.vue
+++ src/views/filetime/filetime.vue
@@ -9,16 +9,25 @@
     <div class="timeline">
       <div class="block">
         <el-timeline class="el_timeline">
-          <el-timeline-item v-for="(value,index) in file" :key="index"  :timestamp="value.time"
+
+          <el-timeline-item v-for="(value,index) in file" :key="index"   :timestamp="value.datetime"
                             placement="top"
                             color="skyblue">
-            <el-card>
-              <h4>{{value.title}}</h4>
-              <p>{{value.content}}</p>
-            </el-card>
+            <router-link :to="`/article/details/${value.id}`" class="filetime_to_article">
+              <el-card>
+                <h4>{{value.title}}</h4>
+              </el-card>
+            </router-link>
+
           </el-timeline-item>
         </el-timeline>
       </div>
+    </div>
+<!--    加载更多-->
+    <div class="loadingButton">
+      <span id="loadingButton">
+        <el-button round @click="loadingMore">加载更多</el-button>
+      </span>
     </div>
 <!--    回到顶部-->
     <backUp></backUp>
@@ -27,41 +36,41 @@
 
 <script>
 import backUp from "@/components/common/backUp/backUp";
+import {mapState} from "vuex";
+
 
 export default {
   name: `filetime`,
   data(){
     return{
-      file:[
-        {
-          title:'四月是你的谎言',
-          time:'2018/4/12',
-          content:"你的名字是？"
-        },
-        {
-          title:'四月是你的谎言',
-          time:'2018/4/12',
-          content:"你的名字是？"
-        },
-        {
-          title:'四月是你的谎言',
-          time:'2018/4/12',
-          content:"你的名字是？"
-        },{
-          title:'四月是你的谎言',
-          time:'2018/4/12',
-          content:"你的名字是？"
-        },
-        {
-          title:'四月是你的谎言',
-          time:'2018/4/12',
-          content:"你的名字是？"
-        },
-      ]
+      page:1,
+      file:''
     }
+  },
+  mounted() {
+    this.file = this.articles[1]
+  },
+  computed:{
+    ...mapState(['articles','article'])
   },
   components:{
     backUp:backUp
+  },
+  methods:{
+    // 加载更多的归档
+    loadingMore(){
+      let pageCount  = 0 | this.article.count/10+1
+      if (this.page >=pageCount){
+        this.$message({
+          type: 'info',
+          message: '没有更多的数据',
+          duration: 1500
+        });
+      }else {
+        this.page = this.page+1
+        this.file.push(...this.articles[this.page])
+      }
+    }
   }
 }
 </script>
@@ -75,4 +84,16 @@ export default {
 .el_timeline {
   padding: 0;
 }
+#loadingButton {
+  text-align: center;
+  display: block;
+}
+.loadingButton {
+
+}
+.filetime_to_article {
+  text-decoration-line: none;
+
+}
+
 </style>
